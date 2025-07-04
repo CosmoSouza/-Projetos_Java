d8f0051eece7099d3ce3665b4bf5b4826467d9dc
CosmoSouza <lucascap9@gmail.com>
2025-07-04 12:06:40 -0300

    Subindo projeto para o GitHub

---
+++ Java_TryCatch/src/Application/Program.java
@@ -0,0 +1,29 @@
+package Application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+		    String[] vect = sc.nextLine().split(" ");
+		    int position = sc.nextInt();
+		    System.out.println(vect[position]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+		}
+		catch (InputMismatchException e) {
+			System.out.println("End of error");
+		}
+		
+		System.out.println("End of program");
+		
+		sc.close();
+	}
+
+}
