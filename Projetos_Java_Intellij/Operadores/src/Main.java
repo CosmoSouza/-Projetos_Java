def6296211eaff217d4e508e406dbcf30f584948
Lucas Souza <lucascao9@gmail.com>
2025-08-09 18:58:15 -0300

    Adicionando Tipos Primitivos

---
+++ Projetos_Java_Intellij/Operadores/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("true  && true = %s \n",   true && true);
+        System.out.printf("false && false = %s \n", false && false);
+        System.out.printf("true  && false = %s \n",  true && false);
+        System.out.printf("false && true = %s \n",  false && true);
+        System.out.println("======================================");
+        System.out.printf("true  || true = %s \n",   true || true);
+        System.out.printf("false || false = %s \n", false || false);
+        System.out.printf("true  || false = %s \n",  true || false);
+        System.out.printf("false || true = %s \n",  false || true);
+        System.out.println("========================================");
+        System.out.printf("!true  = %s \n", !true);
+        System.out.printf("!false = %s \n", !false);
+
+        /*
+        var scanner = new Scanner(System.in);
+        System.out.println("Quantos anos voce tem?");
+        var age = scanner.nextInt();
+        System.out.println("Voce e emancipado?");
+        var isEmancipated = scanner.nextBoolean();
+        var canDrive = age >= 18 || isEmancipated && age >= 16;
+        System.out.printf("Voce pode dirigir? (%s) \n", canDrive);
+         */
+
+        /*
+        var scanner = new Scanner(System.in);
+        System.out.println("Quantos anos voce tem?");
+        var age = scanner.nextInt();
+        var canNotDrive = (age < 18);
+        System.out.printf("Voce pode dirigir? (%s) \n", !canNotDrive);
+         */
+
+          /*
+          var scanner = new Scanner(System.in);
+          System.out.println("Quanto e 2 + 2 ?");
+          var result = scanner.nextInt();
+          var isRight = result == 4;
+          System.out.printf("O resultado e 4, voce acertou? (%s)", isRight);
+         */
+    }
+}
---
+++ Projetos_Java_Intellij/TiposPrimitivos/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //byte number = 1;
+        //short number = 1;
+        //int number = 1;
+        //long number = 1L;
+        //float number = 1.0f;
+        //char character= 'a';
+        //boolean bool = false;
+    }
+}
